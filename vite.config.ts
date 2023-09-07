ea0a6cd91301c5bbdcd1877c2553a750da061159
a <a@a.com>
2023-09-08 01:06:43 +0300

    Fix config

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,9 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-	base: "/redis/",
+	base: process.env.NODE_ENV === "production" ? "/redis" : "/",
+
 	plugins: [
 		react({
 			babel: {
